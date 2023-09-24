f38db536d2ce1924b860bfd94d816224425ad83e
Manuel Arcieri <arcieri@manuel.top>
2023-09-24 19:27:36 +0200

    Fixed PE mapping

--- jobs/hisat2-mapping-PE.sh
+++ jobs/hisat2-mapping-PE.sh
@@ -15,7 +15,7 @@ mkdir --parents "$(dirname "$OUT_FILE")"
 mkdir --parents "$SAM_DIR"
 
 hisat2 --threads "$GEP_N_THREADS" --downstream-transcriptome-assembly --quiet \
-    -x "$HISAT2_INDEX" \
+    -x "$HISAT2_INDEX"/genome \
     -1 "$TRIM_DIR/$GEP_SAMPLE"_1.trimmed.paired.fastq \
     -2 "$TRIM_DIR/$GEP_SAMPLE"_2.trimmed.paired.fastq \
     -U "$TRIM_DIR/$GEP_SAMPLE"_1.trimmed.unpaired.fastq,"$TRIM_DIR/$GEP_SAMPLE"_2.trimmed.unpaired.fastq \
